be19831504c0016e9c515d7c02f313c2c2cdec21
NNEUID <nneuid@gmail.com>
2022-12-26 14:25:50 +0000

    router-links

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home Page</h1>
+   <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates, suscipit nulla iusto esse ipsam expedita obcaecati numquam est, beatae fugit soluta cumque? Maxime doloremque quisquam ipsam rerum deleniti vel at doloribus animi soluta provident cumque laudantium ad dignissimos repellat exercitationem dolor, obcaecati voluptate quia quidem assumenda necessitatibus. Autem sed deserunt tempora nisi quo perferendis quas reiciendis modi cum quisquam ex officia necessitatibus ea, laborum repudiandae expedita, deleniti commodi incidunt facere rem numquam natus rerum. Quis, eveniet suscipit possimus molestias ad porro quo amet aliquam vitae, perspiciatis omnis corporis illo inventore reiciendis quaerat enim veniam. Ut ipsam ullam maiores excepturi dolores.</p>
+   <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates, suscipit nulla iusto esse ipsam expedita obcaecati numquam est, beatae fugit soluta cumque? Maxime doloremque quisquam ipsam rerum deleniti vel at doloribus animi soluta provident cumque laudantium ad dignissimos repellat exercitationem dolor, obcaecati voluptate quia quidem assumenda necessitatibus. Autem sed deserunt tempora nisi quo perferendis quas reiciendis modi cum quisquam ex officia necessitatibus ea, laborum repudiandae expedita, deleniti commodi incidunt facere rem numquam natus rerum. Quis, eveniet suscipit possimus molestias ad porro quo amet aliquam vitae, perspiciatis omnis corporis illo inventore reiciendis quaerat enim veniam. Ut ipsam ullam maiores excepturi dolores.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
   }
 }
 </script>
