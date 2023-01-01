b0798181ea6291d1accfcd1669a0aa0014a69c9b
NNEUID <nneuid@gmail.com>
2023-01-01 23:51:27 +0000

    jobs route and directory

---
+++ src/views/Jobs.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Jobs</h1>
+  <div v-for="job in jobs" :key="job.id">
+    <span>Title: {{ job.title }}</span> <br>
+    <span>Description: {{ job.description }}</span> <br>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      jobs: [
+        { title: 'Job 1', id: 1, description: 'Lorem' },
+        { title: 'Job 2', id: 2, description: 'Lorem' },
+        { title: 'Job 3', id: 3, description: 'Lorem' },
+      ]
+    }
+
+  }
+}
+</script>
+
+<style>
+
+</style>
