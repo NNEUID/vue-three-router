86e041764ad4fe9b31b6c7efca92381ec334ae94
NNEUID <nneuid@gmail.com>
2023-01-02 13:57:36 +0000

    dynamic routes with .params

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import JobsView from '../views/jobs/Jobs.vue'
+import JobDetails from '../views/jobs/JobDetails.vue'
 
 const routes = [
   {
@@ -18,6 +19,11 @@ const routes = [
     path: '/jobs',
     name: 'jobs',
     component: JobsView
+  },
+  {
+    path: '/jobs/:id',
+    name: 'jobdetails',
+    component: JobDetails
   }
 ]
 
